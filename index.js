bd49b5727dbebc5f53b823ff17807210f0ccfd1c
fidelscodes <fidelscodes@gmail.com>
2016-10-15 21:08:55 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+// code to fake Learn test
+console.error('Another lab without a test! Woo!');
+
+window.onload = run;
+
+// actual function that will get called on window load
+function run() {
+  console.log('running...');
+
+  var dodger = document.getElementById('dodger');
+
+  document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+      moveDodgerLeft();
+    }
+
+    if (e.which === 39) {
+      moveDodgerRight();
+    }
+  });
+}
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 5}px`;
+  }
+}
+
